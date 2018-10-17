3b24c33827f41c4c3b73f442a7307cae42e4c459
iceorflame <iceorflame@gmail.com>
2018-10-17 18:43:45 +0300

    Design
    Design

---
+++ FactWorkflow/Controllers/LoginController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FactWorkflow.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+    }
+}
