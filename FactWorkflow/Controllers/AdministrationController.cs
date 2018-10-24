72a33bcb18d1d4b0c46f38d432b90e66738cd5cf
Vadim Duchenko <nelkis.ukr@gmail.com>
2018-10-24 20:11:01 +0300

    Tokens front-end
    Ta pishlo vono vse v sraku! =)

---
+++ FactWorkflow/Controllers/AdministrationController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FactWorkflow.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FactWorkflow.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        private readonly WorkflowContext _context;
+        public AdministrationController(WorkflowContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult TokenTable()
+        {
+            var tokens = _context.Tokens.OrderByDescending(x => x.TId);
+            return View(tokens.ToList());
+        }
+    }
+}
