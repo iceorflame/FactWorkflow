e93f849ce4d8edb94e803127549140dfe8668c81
iceorflame <iceorflame@gmail.com>
2018-10-30 19:22:59 +0200

    Token generator
    Token generator

--- FactWorkflow/Controllers/AdministrationController.cs
+++ FactWorkflow/Controllers/AdministrationController.cs
@@ -16,10 +16,40 @@ namespace FactWorkflow.Controllers
             _context = context;
         }
 
+        private static Random random = new Random();
+        public static string RandomString(int length = 10)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        [HttpGet]
         public IActionResult TokenTable()
         {
             var tokens = _context.Tokens.OrderByDescending(x => x.TId);
             return View(tokens.ToList());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> TokenTable(int tokenCount)
+        {
+            for (int i=0;i < tokenCount; i++)
+            {
+                string tokens = RandomString();
+                Token token = await _context.Tokens.FirstOrDefaultAsync(u => u.TToken == tokens);
+                if (token == null)
+                {
+                    _context.Tokens.Add(new Token { TToken = tokens });
+                }
+                else
+                {
+                    i--;
+                    continue;
+                }
+            }
+            await _context.SaveChangesAsync();
+            return TokenTable();
         }
     }
 }
